1590c0973ebb6f7a908b6ac77a76b8be940f8d3a
soskgo8830 <soskgo@gmail.com>
2024-02-19 06:11:15 +0900

    문제43 : 10진수를 2진수로 문제풀이

---
+++ binaryConversion.js
@@ -0,0 +1,26 @@
+// # 문제43 : 10진수를 2진수로
+
+// 우리가 흔히 사용하는 숫자 1, 8, 19, 28893 등등...은 10진수 체계입니다.
+// 이를 컴퓨터가 알아 들을 수 있는 2진수로 바꾸려고 합니다. 어떻게 해야할까요?
+
+// **사용자에게 숫자를 입력받고 이를 2진수를 바꾸고 그 값을 출력해주세요.**
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  let binaryArray = [];
+  while (line) {
+    binaryArray.push(line % 2);
+    line = parseInt(line / 2);
+  }
+  console.log(binaryArray.reverse().join(''));
+
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
